28509ca93f68b48ca72edf5eb74a4dd846a06cf2
Hector <hamgalex@hotmail.com>
2021-03-17 16:10:48 -0600

    Agregar imágenes de los pokemon y de los tipos.

--- index.js
+++ index.js
@@ -3,15 +3,61 @@ const htmlPokemonListID = document.getElementById("pokemonList");
 //String donde se almacenaran nuestros elementos en forma de html
 let pokemonList = "";
 
+
+// peticion general
 async function requestPokemon(number) {
+
+  
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
   const pokemon = await response.json();
 
+  // url de la imagen del pokemon 
+  let urlimagen="https://www.serebii.net/swordshield/pokemon/"; 
+  // url de la imagen del tipo del pokemon
+  let urlimagentipo="https://www.serebii.net/pokedex-bw/type/" + pokemon.types[0].type.name +".gif";
+  // url de la imagen del tipo secundario (el pokemon puede que no tenga tipo secundario)
+  let urlimagentipo2; 
+
+  // pasar de hectogramos a kilogramos
+  let pesokilos=pokemon.weight/10;
+  // formula para el peso en libras
+  let pesolibras=pesokilos/0.4535923; 
+
+  // Esto es si 0<id<10
+  if(pokemon.id%10==pokemon.id) 
+    urlimagen=urlimagen+"00"+pokemon.id+".png";
+  else
+  // Esto si 10<=id<100
+    if(pokemon.id%100==pokemon.id) 
+      urlimagen=urlimagen+"0"+pokemon.id+".png";
+    else
+      // Esto si 100<=id
+      urlimagen+=pokemon.id+".png"; 
+
+  let canttipos=0;
+   // checa cuantos tipos tiene el pokemon (max 2)
+  for(var prop in pokemon.types)
+  {
+    canttipos++;
+  }
+  
+
   pokemonList += `<tr>
-                    <td scope="row">${pokemon.id}</td>
+                    <td scope="row">${pokemon.id} </td>
                     <td>${pokemon.name}</td>
-                    <td>${pokemon.types[0].type.name}</td>
-                    <td>${pokemon.weight}</td>
+                    <td><img src="${urlimagen}"></td>
+                    <td>
+                      <img src="${urlimagentipo}">`;
+
+  if(canttipos==2) // si tiene 2 tipos entonces pondrá el secundario, de lo contrario no lo coloca
+  {
+    urlimagentipo2= "https://www.serebii.net/pokedex-bw/type/"+ pokemon.types[1].type.name +".gif";
+    pokemonList+=`<img src="${urlimagentipo2}">`;
+  }  
+
+  pokemonList+=` 
+                    </td>
+                    <td>${pesokilos} kgs | ${pesolibras.toFixed(1)} lbs</td>
                 </tr>
                 `;
 }
